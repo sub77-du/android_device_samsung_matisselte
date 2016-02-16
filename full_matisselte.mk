197f1c4e2c17b778ff8afcdba9e6088481a2f0f6
sub77 <sub77@ymail.com>
2016-02-16 12:34:24 +0100

    matisselte: get vendor blobs if exist

--- full_matisselte.mk
+++ full_matisselte.mk
@@ -17,6 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from matissewifi device
 $(call inherit-product, device/samsung/matisselte/device.mk)
+$(call inherit-product-if-exists, vendor/samsung/matisselte/matisselte-vendor.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := matisselte
